b7e57b9a7f26709204f48a63b7e8a40a45b9b6ac
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2022-01-01 18:58:10 +0200

    production config

---
+++ webpack.config.prod.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  performance: {
+    hints: false,
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    // clear: true
+  },
+  // devtool: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './'),
+    },
+    compress: true,
+  },
+  resolve: {
+    extensions: [  // it is an array
+      '.ts',
+      '.js',
+    ],
+  },
+  plugins: [
+      new CleanPlugin.CleanWebpackPlugin()
+  ],
+};
